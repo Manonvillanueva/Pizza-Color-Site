bc4ad25e340bf7959f86eae28ce2fec371450ab2
ManonVillanueva <g.manon14@gmail.com>
2024-11-10 18:49:49 +0100

    header and newsletter style

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,43 @@
 import React from "react";
 import "../Style/header.css";
 import logoPizza from "../assets/img/logo-pizza.png";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ displayNav }) => {
+  // Récupère le chemin d'URL actuel pour appliquer un style conditionnel sur les spans et sur le bg du header
+  const location = useLocation();
+
+  // Variable pour changer la couleur du background des spans du menu hamburger
+  const backgroundSpan =
+    location.pathname === "/contact" ? "var(--ocre)" : "var(--yellow)";
+
   return (
-    <div className="header-container">
-      <img className="logo-pizza" src={logoPizza} alt="logo pizza" />
-      {/* Le bouton "hamburger", qui déclenche l'affichage ou le masquage de la navigation */}
-      <button className="hamburger-menu" onClick={displayNav}>
-        {/* Trois spans pour styliser le menu hamburger  */}
-        <span className="hamburger-bar"></span>
-        <span className="hamburger-bar"></span>
-        <span className="hamburger-bar"></span>
-      </button>
+    <div
+      className="header-color"
+      style={{
+        background:
+          location.pathname === "/contact" ? "var(--yellow)" : "var(--ocre)",
+      }}
+    >
+      <div className="header-container">
+        <img className="logo-pizza" src={logoPizza} alt="logo pizza" />
+        {/* Le bouton "hamburger", qui déclenche l'affichage ou le masquage de la navigation */}
+        <button className="hamburger-menu" onClick={displayNav}>
+          {/* Trois spans pour styliser le menu hamburger  */}
+          <span
+            style={{ background: backgroundSpan }}
+            className="hamburger-bar"
+          ></span>
+          <span
+            style={{ background: backgroundSpan }}
+            className="hamburger-bar"
+          ></span>
+          <span
+            style={{ background: backgroundSpan }}
+            className="hamburger-bar"
+          ></span>
+        </button>
+      </div>
     </div>
   );
 };
--- src/Components/Newsletter.js
+++ src/Components/Newsletter.js
@@ -1,22 +1,33 @@
 import React from "react";
 import imgNewsletter from "../assets/img/basilic and pizza.jpeg";
+import "../Style/newsletter.css";
 
 const Newsletter = () => {
   return (
     <div className="newsletter-container">
+      {/* LEFT PART  */}
       <div className="left-part-news">
-        <h2>Newsletter</h2>
-        <p>Nulla consequat massa quis enim. Donec pede justo fringilla vel.</p>
-        <p>
-          Excellent pizza at a fair price. The restaurant does delivery and has
-          some tables to stay. It's not big but worth to buy pizza there. The
-          pleasure of having a pizza that just left the wood oven is awesome!
-        </p>
-        <input type="email" name="" id="" />
-        <button type="submit">submit</button>
+        <div className="newsletter">
+          <h2>Newsletter</h2>
+          <p>
+            Nulla consequat massa quis enim. Donec pede justo fringilla vel.
+          </p>
+          <p>
+            Excellent pizza at a fair price. The restaurant does delivery and
+            has some tables to stay. It's not big but worth to buy pizza there.
+            The pleasure of having a pizza that just left the wood oven is
+            awesome!
+          </p>
+          <form className="submit-container">
+            <input type="email" placeholder="Enter a valid email address" />
+            <button type="submit">submit</button>
+          </form>
+        </div>
       </div>
+
+      {/* RIGHT PART  */}
       <div className="right-part-news">
-        <img src={imgNewsletter} alt="pizza" />
+        <img src={imgNewsletter} alt="pizza" className="img-newsletter" />
       </div>
     </div>
   );
